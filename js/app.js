e16bcca599bb2104a1b679b7b0030b57d4d96fd3
Stefanpgr <stefanpongrz@gmail.com>
2019-07-17 02:59:00 +0100

    added link to text

--- js/app.js
+++ js/app.js
@@ -5,3 +5,12 @@ if ("serviceWorker" in navigator) {
     });
   });
 }
+
+// function order() {
+//   let num = 0705463667;
+//   let inj = `<a href="sms://${num}?body=I want to order..."class="btn btn-primary">Go somewhere</a>`;
+//   const myFunction = () => {
+//     document.getElementById("add_message").innerHTML = inj;
+//   };
+//   myFunction();
+// }
