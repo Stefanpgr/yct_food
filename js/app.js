910affc9527f39b0de6f4eb4655a3875866dfd08
Stefanpgr <stefanpongrz@gmail.com>
2019-07-19 13:54:48 +0100

    UI updates and added pages

--- js/app.js
+++ js/app.js
@@ -14,3 +14,22 @@ if ("serviceWorker" in navigator) {
 //   };
 //   myFunction();
 // }
+// let btnContainer = document.getElementById("navbar-nav");
+
+// // Get all buttons with class="btn" inside the container
+// var btns = btnContainer.getElementsByClassName("nav-item");
+
+// // Loop through the buttons and add the active class to the current/clicked button
+// for (var i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", function() {
+//     var current = document.getElementsByClassName("active");
+//     current[0].className = current[0].className.replace(" active", "");
+//     this.className += " active";
+//   });
+// }
+$(document).ready(function() {
+  $("div a").click(function() {
+    $("div a").removeClass("active");
+    $(this).addClass("active");
+  });
+});
