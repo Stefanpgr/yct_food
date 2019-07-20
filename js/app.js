54020fbe3c1adc8d2fed64a5ae003defef7523c9
Stefanpgr <stefanpongrz@gmail.com>
2019-07-20 23:16:54 +0100

    bug fixes

--- js/app.js
+++ js/app.js
@@ -9,19 +9,19 @@ if ("serviceWorker" in navigator) {
 const getFormData = () => {
   let orderItem = document.getElementById("orderItem").value;
   let orderLocale = document.getElementById("locale").value;
-  let orderBtn = `<a href="sms://08097463636?body=I want to Order: ${orderItem} Location: ${orderLocale}"><button onclick="getFormData()"  type="button" class="btn btn-success btn-sm">
-              Text Now!
-            </button></a>`;
+  let orderBtn = `<button onclick="getFormData()" type="button" class="btn btn-success btn-sm"> <a  href="sms://08097463636?body=I want to Order: ${orderItem} Location: ${orderLocale}">Text Now!   </a> </button>`;
   document.getElementById("order").innerHTML = orderBtn;
-  // document.getElementById("orderItem").value = "2";
-  // document.getElementById("locale").value = "1";
+  document.getElementById("orderItem").value = "";
+  document.getElementById("locale").value = "";
   console.log(orderItem, orderLocale);
 };
-
+getFormData();
+const addData = () => {
+  getFormData();
+};
 const item1 = () => {
   let title = document.getElementById("item1").textContent;
   document.getElementById("title").innerHTML = title;
-  getFormData();
 };
 
 const item2 = () => {
