553e2e1232bba638cf80ff132d0bae2c80480899
stefanpgr <stefanpongrz@gmail.com>
2019-08-07 11:19:47 +0100

    added cards

--- js/app.js
+++ js/app.js
@@ -6,13 +6,24 @@ if ("serviceWorker" in navigator) {
   });
 }
 
+const numObj = {
+  nikki: "08072773053",
+  adex: " 08037863809",
+  sam: "08135053968",
+  ff: "08130845097",
+  bukola: "08035062445"
+};
+
+// console.log(vendorNum[0]);
 const getFormData = () => {
+  console.log(vendorNum, "check");
   let orderItem = document.getElementById("orderItem").value;
   let orderLocale = document.getElementById("locale").value;
+
   // let orderBtn = `sms://08097463636?body=I want to Order: ${orderItem} Location: ${orderLocale}`;
   document.getElementById(
     "txr"
-  ).href = `sms://08097463636?body=I want to Order: ${orderItem} Location: ${orderLocale}`;
+  ).href = `sms://${vendorNum}?body=I want to Order: ${orderItem} Location: ${orderLocale}`;
   document.getElementById("orderItem").value = "";
   document.getElementById("locale").value = "";
   console.log(orderItem, orderLocale);
@@ -24,13 +35,64 @@ const addData = () => {
 const item1 = () => {
   let title = document.getElementById("item1").textContent;
   document.getElementById("title").innerHTML = title;
+  vendorNum = Number(numObj.nikki);
+  getFormData(vendorNum);
+
+  return vendorNum;
 };
 
 const item2 = () => {
-  let title = document.getElementById("item2").textContent;
-  document.getElementById("title").innerHTML = title;
-  getFormData();
+  let title = document.getElementById("item2").textContent; // collects card title
+  document.getElementById("title").innerHTML = title; //modal title
+  vendorNum = Number(numObj.adex);
+  getFormData(vendorNum);
+
+  return vendorNum;
 };
+// console.log(num);
+// const item2 = () => {
+//   let title = document.getElementById("item2").textContent; // collects card title
+//   document.getElementById("title").innerHTML = title; //modal title
+//   const vendorNum = numObj.adex;
+//   getFormData(vendorNum);
+
+//   return vendorNum;
+// };
+// console.log(vendorNum, "item2", item2());
+const item3 = () => {
+  let title = document.getElementById("item3").textContent; // collects card title
+  document.getElementById("title").innerHTML = title; //modal title
+  vendorNum = Number(numObj.bukola);
+  getFormData(vendorNum);
+
+  return vendorNum;
+};
+
+const item4 = () => {
+  let title = document.getElementById("item4").textContent; // collects card title
+  document.getElementById("title").innerHTML = title; //modal title
+  vendorNum = Number(numObj.ff);
+  getFormData(vendorNum);
+
+  return vendorNum;
+};
+
+const item5 = () => {
+  let title = document.getElementById("item5").textContent; // collects card title
+  document.getElementById("title").innerHTML = title; //modal title
+  getFormData();
+  vendorNum = Number(numObj.sam);
+  getFormData(vendorNum);
+
+  return vendorNum;
+};
+// if (item1) {
+//   vendorNum = numObj.nikki;
+// } else if (item2) {
+//   vendorNum = numObj.adex;
+// }
+// console.log(vendorNum, "he");
+
 // test();
 // $(document).ready(function() {
 //   $("nav div > div > a").click(function() {
