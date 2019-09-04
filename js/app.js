d8565fc91981f28aef6c7987b3e7d56305f53a12
Stefanpgr <stefanpongrz@gmail.com>
2019-09-04 09:25:01 +0100

    added features and fix bugs

--- js/app.js
+++ js/app.js
@@ -17,34 +17,42 @@ const numObj = {
 
 // console.log(vendorNum[0]);
 const getFormData = () => {
-  // console.log(vendorNum, "check");
-  const orderItem = document.getElementById("orderItem").value;
   const orderLocale = document.getElementById("locale").value;
   select = document.getElementById("menuItemList");
-  const soup = select.options[select.selectedIndex].text;
-  console.log(soup);
+  const selectedItem = select.options[select.selectedIndex].text;
+  console.log(selectedItem);
   console.log(vendorNum);
   const isInvalid = "";
-  console.log(typeof orderItem);
-  // let orderBtn = `sms://08097463636?body=I want to Order: ${orderItem} Location: ${orderLocale}`;
-  if ((orderItem && orderLocale) === isInvalid) {
+
+  selectedSwal = document.querySelector("input[name='swal']:checked");
+  if (selectedSwal) {
+    console.log(selectedSwal.value);
+  }
+
+  if (orderLocale === isInvalid) {
     alert("invalid input, Pls check input fields");
+  } else if (selectedSwal) {
+    document.getElementById(
+      "txr"
+    ).href = `sms://${vendorNum}?body=Order: ${selectedItem} and ${selectedSwal.value} Location: ${orderLocale}`;
+    console.log(selectedItem, orderLocale);
+
+    document.getElementById("locale").value = "";
   } else {
     document.getElementById(
       "txr"
-    ).href = `sms://${vendorNum}?body=I want to Order: ${orderItem} Location: ${orderLocale}`;
-    console.log(orderItem, orderLocale);
-    document.getElementById("orderItem").value = "";
+    ).href = `sms://${vendorNum}?body=Order: ${selectedItem} Location: ${orderLocale}`;
+    console.log(selectedItem, orderLocale);
+
     document.getElementById("locale").value = "";
   }
 };
 
-const createMenuItems = (ul) =>{
+const createMenuItems = ul => {
   select = document.getElementById("menuItemList");
-  
-  
+
   let menuItems = ul.getElementsByTagName("li");
-  
+
   for (let i = 0; i < menuItems.length; i++) {
     // Create the list item:
     let item = document.createElement("option");
@@ -55,35 +63,32 @@ const createMenuItems = (ul) =>{
     // Add it to the list:
     select.appendChild(item);
   }
-}
+};
 
 // checks for and removes item created to prevent other menu items from adding up
-const removeMenuItems = () =>{
- 
+const removeMenuItems = () => {
   select = document.getElementById("menuItemList");
   if (select.hasChildNodes()) {
-    select.innerHTML= "";
+    select.innerHTML = "";
   }
-}
-
+};
 
 const item1 = () => {
   let title = document.getElementById("item1").textContent;
   document.getElementById("title").innerHTML = title;
   vendorNum = Number(numObj.nikki);
   let ul = document.getElementById("menu1");
-  removeMenuItems()
+  removeMenuItems();
   createMenuItems(ul);
   return vendorNum;
 };
-
 
 const item2 = () => {
   let title = document.getElementById("item2").textContent; // collects card title
   document.getElementById("title").innerHTML = title; //modal title
   vendorNum = Number(numObj.adex);
   let ul = document.getElementById("menu2");
-  removeMenuItems()
+  removeMenuItems();
   createMenuItems(ul);
   return vendorNum;
 };
@@ -93,7 +98,7 @@ const item3 = () => {
   document.getElementById("title").innerHTML = title; //modal title
   vendorNum = Number(numObj.bukola);
   let ul = document.getElementById("menu3");
-  removeMenuItems()
+  removeMenuItems();
   createMenuItems(ul);
   return vendorNum;
 };
@@ -103,7 +108,7 @@ const item4 = () => {
   document.getElementById("title").innerHTML = title; //modal title
   vendorNum = Number(numObj.sam);
   let ul = document.getElementById("menu4");
-  removeMenuItems()
+  removeMenuItems();
   createMenuItems(ul);
   return vendorNum;
 };
@@ -113,8 +118,9 @@ const item5 = () => {
   document.getElementById("title").innerHTML = title; //modal title
   vendorNum = Number(numObj.ewaNaB);
   let ul = document.getElementById("menu5");
+  removeMenuItems();
   createMenuItems(ul);
-  removeMenuItems()
+
   return vendorNum;
 };
 
@@ -123,8 +129,9 @@ const item6 = () => {
   document.getElementById("title").innerHTML = title; //modal title
   vendorNum = Number(numObj.ff);
   let ul = document.getElementById("menu6");
+  removeMenuItems();
   createMenuItems(ul);
-  removeMenuItems()
+
   return vendorNum;
 };
 
